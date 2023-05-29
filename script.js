16dbf3b928315353f65cd164f29d1bf9982bd9f5
cheval-constipe <nilsenoddw@gmail.com>
2023-05-29 12:01:48 +0200

    separate navbar and footer

---
+++ script.js
@@ -0,0 +1,43 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener('scroll', function () {
+        var navbar = document.querySelector('.navbar');
+        if (window.scrollY > 10) {
+            navbar.classList.add('navbar-scrolled');
+        } else {
+            navbar.classList.remove('navbar-scrolled');
+        }
+    });
+
+    $(document).ready(function () {
+        $(".navbar-nav li a").click(function (event) {
+            $(".navbar-collapse").collapse('hide');
+        });
+    });
+
+    // Fetch the content of navbar.html
+    fetch("navbar.html")
+        .then(function (response) {
+            return response.text();
+        })
+        .then(function (html) {
+            // Insert the navbar HTML into the placeholder div
+            document.getElementById("navbar-placeholder").innerHTML = html;
+        })
+        .catch(function (error) {
+            console.log("Error fetching navbar.html:", error);
+        });
+
+    // Fetch the content of footer.html
+    fetch("footer.html")
+        .then(function (response) {
+            return response.text();
+        })
+        .then(function (html) {
+            // Insert the footer HTML into the placeholder div
+            document.getElementById("footer-placeholder").innerHTML = html;
+        })
+        .catch(function (error) {
+            console.log("Error fetching footer.html:", error);
+        });
+});

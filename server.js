fee281f3b848ee8de49e1e2c4d8de811040832f8
cheval-constipe <nilsenoddw@gmail.com>
2023-06-06 21:01:07 +0200

    Search done

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const cheerio = require('cheerio');
+const { JSDOM } = require('jsdom');
 
 const app = express();
 const port = 3000;
@@ -27,12 +27,7 @@ app.get('/getArticles', (req, res) => {
 });
 
 app.get('/searchArticles', (req, res) => {
-    const keyword = req.query.keyword;
-
-    if (!keyword) {
-        res.json([]);
-        return;
-    }
+    const searchTerm = req.query.term; // Changed 'query' to 'term'
 
     const articlesDir = path.join(__dirname, 'artikler');
     fs.readdir(articlesDir, (err, files) => {
@@ -42,21 +37,38 @@ app.get('/searchArticles', (req, res) => {
             return;
         }
 
+        const articleFiles = files.filter(file => file.endsWith('.html') && file !== 'index.html');
+
         const searchResults = [];
 
-        files.forEach(file => {
-            if (file.endsWith('.html') && file !== 'index.html') {
-                const filePath = path.join(articlesDir, file);
-                const fileContent = fs.readFileSync(filePath, 'utf8');
-                const $ = cheerio.load(fileContent);
+        articleFiles.forEach(file => {
+            const filePath = path.join(articlesDir, file);
+            const fileContent = fs.readFileSync(filePath, 'utf-8');
 
-                const articleText = $('h1, h2, p').text();
-                if (articleText.toLowerCase().includes(keyword.toLowerCase())) {
-                    searchResults.push({
-                        file: file,
-                        matches: [file, $('h2').text()]
-                    });
-                }
+            const { document } = new JSDOM(fileContent).window;
+
+            const titleElement = document.querySelector('h1');
+            const subtitleElement = document.querySelector('h2');
+            const contentElement = document.querySelector('.article-text');
+            const imageElement = document.querySelector('img');
+
+            const title = titleElement ? titleElement.textContent : '';
+            const subtitle = subtitleElement ? subtitleElement.textContent : '';
+            const content = contentElement ? contentElement.innerHTML.replace(/^<p>|<\/p>$/g, '') : '';
+            const imageUrl = imageElement ? imageElement.src : '';
+
+            if (
+                title.toLowerCase().includes(searchTerm.toLowerCase()) || // Changed 'searchQuery' to 'searchTerm'
+                subtitle.toLowerCase().includes(searchTerm.toLowerCase()) || // Changed 'searchQuery' to 'searchTerm'
+                content.toLowerCase().includes(searchTerm.toLowerCase()) // Changed 'searchQuery' to 'searchTerm'
+            ) {
+                searchResults.push({
+                    file,
+                    title,
+                    subtitle,
+                    content,
+                    imageUrl
+                });
             }
         });
 
